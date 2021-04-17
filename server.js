df594d6fe3f4bda429469990ec680fb04af462d1
Mickey Perlstein <corp.mp@gmail.com>
2021-04-17 14:17:35 +0300

    new update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Awsome eur-to-usd')
+    res.end('Hello Awsome eur-to-usd v2')
 })
 
 const PORT = process.env.PORT || 8080
