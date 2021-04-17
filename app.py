43e61697b6147451cbba8d2ce5145d38ae1b9950
mickeyP <corp.mp@gmail.com>
2021-04-17 19:34:37 +0300

    after crash

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask
 from flask import render_template, request
-from currency_converter import CurrencyConverter
 
 app = Flask(__name__)
 
@@ -14,16 +13,15 @@ def form():
 
 @app.route("/", methods=["POST"])
 def my_form_post():
-    c = CurrencyConverter()
 
     euros = request.form["euros"]
-    usd = round(c.convert(euros, "EUR", "USD"), 2)
+    usd = round(2.25, 2)
 
     return render_template("form.html", euros=euros, usd=usd)
 
 
 if __name__ == "__main__":
-    
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    is_dev = os.environ.get('USERNAME', 'root') != 'root'
+    app.run(debug=is_dev, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 
